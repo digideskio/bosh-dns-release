783953f4c567c171888a5a5cd075efb60cdf5f78
Josh Russett <jrussett@pivotal.io>
2018-03-13 10:54:21 -0700

    Configure pipeline with new test stress job configuration
    Signed-off-by: Michael Xu <mxu@pivotal.io>

---
+++ ci/configure_test_stress.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+  lpass status
+set +ex
+
+this_dir=$(dirname $0)
+
+fly -t ${CONCOURSE_TARGET:-production} sp -p new-test-stress-pipeline -c $this_dir/test_stress_pipeline.yml --load-vars-from <(lpass show 'dns-release pipeline vars' --notes)
